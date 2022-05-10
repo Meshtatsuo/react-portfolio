4ab9bf660f15645912a9114b5a9bc2ae2f325cf7
meshtatsuo <meshtatsuo@gmail.com>
2022-05-09 19:46:10 -0600

    project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
+  // Nav props
+  const [pages] = useState([
+    {
+      name: "ABOUT",
+      description: "About me, link to my resume",
+    },
+    {
+      name: "PORTFOLIO",
+      description: "A collection of my development work.",
+    },
+    {
+      name: "CONTACT",
+      description: "Get in touch!",
+    },
+  ]);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  // Showcase item or items
+  const [showcase_items] = useState([
+    {
+      title: "Photo Reviewer",
+      description: "A web application for sharing photos with clients.",
+      tech: "NodeJS | ExpressJS | Sequelize | AWS",
+      link: "https://fomo-photo-reviewer.herokuapp.com/",
+      img: `${require("./assets/portfolio-items/PI_PhotoReviewer.jpg")}`,
+      alt: "photo reviewer thumbnail",
+    },
+  ]);
+  // Portfolio Pieces
+  const [portfolio_items] = useState([
+    {
+      title: "Mental Health Check",
+      description: "A website for personal mental health check ins!",
+      tech: "HTML | CSS | JS",
+      link: "https://nitrotap.github.io/mental-health-check/",
+      img: `${require("./assets/portfolio-items/mental-health-check.png")}`,
+      alt: "Mental Health Check Thumbnail",
+    },
+    {
+      title: "Llama Player",
+      description: "A cross platform music player!",
+      tech: "NodeJS | Electron | HowlerJS",
+      link: "https://github.com/Meshtatsuo/llamaPlayer",
+      img: `${require("./assets/portfolio-items/llama-player.png")}`,
+      alt: "Llama Player thumbnail",
+    },
+    {
+      title: "The Medician Blog",
+      description:
+        "A fictional blog site built around a post noir audio drama world.",
+      tech: "NodeJS | MySQL | ExpressJS",
+      link: "https://medician-blog.herokuapp.com/",
+      img: `${require("./assets/portfolio-items/medician-blog.png")}`,
+      alt: "Medician Blog Thumbnail",
+    },
+    {
+      title: "THX Deep Note Recreation",
+      description:
+        "Recreation of the THX deep note generated in real time using Unreal Engine 5.",
+      tech: "Unreal Engine 5",
+      link: "https://vimeo.com/662102223",
+      img: `${require("./assets/portfolio-items/thx-ue5.png")}`,
+      alt: "THX Deep Note Thumbnail",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      ></Nav>
+      <Hero></Hero>
+      <main>
+        {currentPage === "HOME" && <Home />}
+        {currentPage === "ABOUT" && <About />}
+        {currentPage === "PORTFOLIO" && (
+          <Portfolio
+            portfolio_items={portfolio_items}
+            showcase_items={showcase_items}
+          ></Portfolio>
+        )}
+        {currentPage === "CONTACT" && <Contact />}
+      </main>
     </div>
   );
 }
